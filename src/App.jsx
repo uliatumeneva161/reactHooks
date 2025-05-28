a75e57c771329f29c787c24701db221726094e34
julia <julia_gtyumenevaya_011022@mail.ru>
2025-05-28 10:32:55 +0300

    Закреплена шапка

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,38 @@ import { useState } from 'react';
 import './App.css';
 import UseReducer1 from './components/UseReducer1';
 import UseReducer2 from './components/UseReducer2';
+
 function App() {
-  const [currentPage, setCurrentPage] = useState(null); 
+  const [currentPage, setCurrentPage] = useState(null);
 
   function nextPage(namePage) {
     setCurrentPage(namePage);
   }
 
   return (
-    <div>
-      <nav>
-         <p>Учебные примеры по хукам жизненного цикла в React</p>
+    <div className="app-container">
+      <nav className="fixed-navbar">
+        <div className="bl">
+          <b>useReduce</b>
+          <a 
+            className="nav-link" 
+            onClick={() => nextPage('useReducer1')}
+          >
+            Корзина товаров
+          </a>
+          <a 
+            className="nav-link" 
+            onClick={() => nextPage('useReducer2')}
+          >
+            Форма регистрации
+          </a>
+        </div>
       </nav>
-      {/* <select value='UseReducer'>
-        <option value=""><a onClick={() => nextPage('useReducer1')}>Корзина товаров</a></option>
-        <option value=""><a onClick={() => nextPage('useReducer2')}>Форма регистрации</a></option>
-      </select> */}
-      <div className='bl'>
-        <b>useReduce</b>
-         <a onClick={() => nextPage('useReducer1')}>Корзина товаров</a>
-          <a onClick={() => nextPage('useReducer2')}>Форма регистрации</a>
+
+      <div className="content">
+        {currentPage === 'useReducer1' && <UseReducer1 />}
+        {currentPage === 'useReducer2' && <UseReducer2 />}
       </div>
-     
-      {currentPage === 'useReducer1' && <UseReducer1 />}
-      {currentPage === 'useReducer2' && <UseReducer2 />}
     </div>
   );
 }

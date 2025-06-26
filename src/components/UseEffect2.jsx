ac34678ac7cc9a03b72d7643a1e786e08c847bfe
julia <julia_gtyumenevaya_011022@mail.ru>
2025-06-26 22:29:40 +0300

    add desc

--- src/components/UseEffect2.jsx
+++ src/components/UseEffect2.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import Description from './Description';
 export default function UseEffect2() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -24,6 +24,14 @@ export default function UseEffect2() {
   
   return (
     <div>
+      <Description children='useEffect здесь используется для выполнения 
+      побочного эффекта (загрузки данных с API) только один раз при монтировании
+       компонента, что обеспечивается пустым массивом зависимостей [] во втором
+       аргументе, при этом внутри эффекта объявляется и вызывается асинхронная
+       функция fetchData, которая: Устанавливает состояние загрузки (setLoading(true) неявно через начальное
+        значение) Выполняет запрос к GitHub API Обновляет состояние данными (setData)
+        Снимает флаг загрузки в finally блоке (гарантированно даже при ошибке)
+        Возвращает разметку с условиями: индикатор загрузки или отображение данных.'/>
       {data ? <pre>{JSON.stringify(data, 0, 2)}</pre> : 'Данные не получены'}
     </div>
   );

de4f1a20f9885238fb58b93ed2a478d4cae8aa2e
julia <julia_gtyumenevaya_011022@mail.ru>
2025-07-03 17:03:46 +0300

    03-07

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const app = express();
-const PORT = 3001;
+const express = require('express')
+const app = express()
 
-// Жестко запрограммированные данные телефонной книги
+// Данные телефонной книги
 const persons = [
   { 
     id: "1",
@@ -24,14 +23,47 @@ const persons = [
     name: "Mary Poppendieck", 
     number: "39-23-6423122"
   }
-];
+]
 
-// Роут для получения записей телефонной книги
-app.get('/api/persons', (req, res) => {
-  res.json(persons);
-});
+// Маршрут для получения всех записей (упражнение 3.1)
+app.get('/api/persons', (_, response) => {
+  response.json(persons)
+})
 
-// Запуск сервера
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Маршрут для информации (упражнение 3.2)
+app.get('/info', (_, response) => {
+  const currentDate = new Date()
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${currentDate}</p>
+  `)
+})
+app.get('/julia', (_, response) => { 
+  const pr = 2+3
+  response.send(`
+    <h1>Julia ${pr}</h1>
+    `)
+})
+// Маршрут для получения одной записи
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find(p => p.id === id);
+
+  if (person) {
+    const t = 2+2
+    response.json(person);
+    app.send(`<h1>${t}</h1>`)
+  } else {
+    response.status(404).end(); // 404 Not Found
+  }
 });
+// const PORT = 3001
+// app.listen(PORT, () => {
+//   console.log(`Server on port ${PORT}, Jul`)
+// })
+
+app.listen(3001, () => {
+  // alert('jkjkjkj')
+  console.log('hjjh');
+  
+})
